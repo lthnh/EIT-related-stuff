ea432b8a774f9b0656fbeb07e430bcddc177a589
lthnh <imr3ng4l@gmail.com>
2023-03-04 11:31:48 +0700

    Save images to local folder

--- EIT.m
+++ EIT.m
@@ -13,7 +13,7 @@ for i = 2:num_of_meas
     imdl.fwd_model.stimulation = stim;
     imdl.fwd_model.meas_select = meas_select;
 % x = zeros([256 1]); y = zeros([256 1]);
-% j = 1;
+% j = 1;----------------------------------------
 % for i = 1:256
 %     if meas_select(i)
 %         x(i) = data_homg(j);
@@ -23,10 +23,16 @@ for i = 2:num_of_meas
 % end
 % clear j;
     img = inv_solve(imdl, x, y);
-% img.calc_colours.cb_shrink_move = [0.5,0.8,0.02];
-% img.calc_colours.ref_level = 0;
+%     img.calc_colours.cb_shrink_move = [0.5,0.8,0.02];
+%     img.calc_colours.ref_level = 0;
+    
 % clf; show_fem(img,[1,1]); axis off; axis image
-%     operator_name = input("Enter operator's name: ");
-%     img_name = strcat(string(datetime),operator_name,'.png');
+    date_current_as_string = string(datetime('now','Format','dd-MM-yyyy'));
+    mkdir(date_current_as_string); % Create a directory with current date as its name
+    cd(date_current_as_string);
+    img_name = strcat(string(i-1),'.png');
     figure; show_slices(img);
+    print_convert(img_name);
+    close;
+    cd ..
 end
